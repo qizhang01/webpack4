f4bb7aaa8de5be81619562e1bfa28ef43d47b63b
qizhang <zhishui120@sina.com>
2022-10-04 16:59:26 +0800

    工资前端实现

--- src/pages/salary/index.tsx
+++ src/pages/salary/index.tsx
@@ -1,17 +1,7 @@
 import React, { useState } from 'react'
-import {
-    Form,
-    Select,
-    Input,
-    InputNumber,
-    Button,
-    Row,
-    Col,
-    message,
-} from 'antd'
+import { Form, Select, Input, InputNumber, Button, Row, Collapse, message } from 'antd'
 import { UploadOutlined, InboxOutlined } from '@ant-design/icons'
 import { Panel } from '@/components/Panel'
-import { useLocation } from 'react-router-dom'
 import './index.less'
 import { Auth } from '@/auth'
 import fetchApi from '@/ajax/index'
@@ -20,7 +10,7 @@ const { Option } = Select
 
 const formItemLayout = {
     labelCol: { span: 4 },
-    wrapperCol: { span: 8 },
+    wrapperCol: { span: 16 },
 }
 
 const stl = {
@@ -29,46 +19,108 @@ const stl = {
 const InputNumberStl = { width: '60%' }
 
 const PageSub: React.FC = () => {
-    const param = useLocation()
-
     const [method, setMethod] = useState('日结')
 
     const onFinish = async (values: any) => {
         console.log('Received values of form: ', values)
-        // name,goods_type,goods_no,model_type,price,unit,ifopen,goods_prod_address,buy_date,buy_number,store_house,delivery_address,ifdelete
-        const {
-            name,
-            goods_type = '',
-            goods_no,
-            model_type = '',
-            price,
-            unit = '',
-            ifopen = 1,
-            goods_prod_address = '',
-            buy_date = '',
-            buy_number = 0,
-            store_house = '',
-            delivery_address = '',
-        } = values
-        const body = {
-            name,
-            goods_type,
-            goods_no,
-            model_type,
-            price,
-            unit,
-            goods_prod_address,
-            buy_number,
-            store_house,
-            delivery_address,
-            ifdelete: 0,
-            userno: Auth.loginInfo.id,
+        let body = {}
+        let path = ''
+        let totalSalary
+        const { salarytpye } = values
+        if (salarytpye === '日结') {
+            const {
+                salarytpye,
+                name,
+                emplyeeid,
+                workday,
+                daytotal,
+                daysalary,
+                worklong,
+                worklongmoney,
+            } = values
+            totalSalary = workday * (daysalary + (worklong * worklongmoney) / daytotal)
+            body = {
+                salarytpye,
+                name,
+                emplyeeid,
+                workday,
+                daytotal,
+                daysalary,
+                worklong,
+                worklongmoney,
+                totalSalary,
+                userno: Auth.loginInfo.id,
+            }
+            path = 'savesimpleday'
+        } else if (salarytpye === '月结') {
+            const {
+                salarytpye,
+                name,
+                emplyeeid,
+                workday,
+                daytotal,
+                fullsalary,
+                daysalary,
+                overdaysalary,
+                factrestdays,
+            } = values
+            path = 'savesimpleday'
+        } else if (salarytpye === '无满勤奖励') {
+            const {
+                salarytpye,
+                name,
+                emplyeeid,
+                workday,
+                holidays,
+                monthholiday,
+                factrestdays,
+                daysalary,
+            } = values
+            totalSalary = daysalary * (workday + holidays + monthholiday - factrestdays)
+            body = {
+                salarytpye,
+                name,
+                emplyeeid,
+                workday,
+                daysalary,
+                totalSalary,
+                monthholiday,
+                userno: Auth.loginInfo.id,
+            }
+            path = 'savesimplemonth'
+        } else if (salarytpye === '按比例核定') {
+            const {
+                salarytpye,
+                name,
+                emplyeeid,
+                workday,
+                holidays,
+                monthholiday,
+                factrestdays,
+                daysalary,
+            } = values
+            totalSalary =
+                ((workday * factrestdays) / 28).toFixed() +
+                daysalary * (workday + holidays + monthholiday - factrestdays)
+            body = {
+                salarytpye,
+                name,
+                emplyeeid,
+                workday,
+                daysalary,
+                totalSalary,
+                monthholiday,
+                userno: Auth.loginInfo.id,
+            }
+            path = 'savesimplemonth'
         }
+
         console.log(body)
-        const result = await fetchApi('api/addOneProduct', JSON.stringify(body), 'POST')
+
+        const result = await fetchApi(`api/salary/${path}`, JSON.stringify(body), 'POST')
         console.log(result)
         if (result.code == '200') {
-            message.info('提交成功')
+            message.info(`提交成功, 薪水为${totalSalary}`)
         } else {
             message.info('提交失败, 请重新提交')
         }
@@ -78,94 +130,172 @@ const PageSub: React.FC = () => {
         setMethod(value)
     }
 
+    const calSalary = (type: string) => {}
+
     return (
         <Panel>
-            <Form name="config-form" {...formItemLayout} onFinish={onFinish}>
-                <Form.Item name="type" label="核算方式">
-                    <Select defaultValue="日结" style={{ width: 120 }} onChange={selectMethod}>
-                        <Option value="日结">日结</Option>
-                        <Option value="月结">月结</Option>
-                        <Option value="无满勤奖励">无满勤奖励</Option>
-                        <Option value="按比例核定">按比例核定</Option>
-                        <Option value="核定后扣除">核定后扣除</Option>
-                    </Select>
-                </Form.Item>
-                <div style={{ display: method == '日结' ? 'block' : 'none' }}>
-                    <Form.Item name="workday" label="本月出勤">
-                        <InputNumber placeholder="请输入本月出勤天数" />
-                    </Form.Item>
+            <Collapse>
+                <Collapse.Panel header="员工薪水录入" key="1">
+                    <Form name="config-form" {...formItemLayout} onFinish={onFinish}>
+                        <Form.Item name="salarytpye" label="核算方式">
+                            <Select
+                                defaultValue="日结"
+                                style={{ width: 120 }}
+                                onChange={selectMethod}
+                            >
+                                <Option value="日结">日结</Option>
+                                {/* <Option value="月结">月结</Option> */}
+                                <Option value="无满勤奖励">无满勤奖励</Option>
+                                <Option value="按比例核定">按比例核定</Option>
+                                {/* <Option value="核定后扣除">核定后扣除</Option> */}
+                            </Select>
+                        </Form.Item>
+                        <Form.Item
+                            name="name"
+                            label="姓名"
+                            rules={[{ required: true, message: '必须输入姓名' }]}
+                        >
+                            <Input placeholder="请输入姓名" />
+                        </Form.Item>
+                        <Form.Item
+                            name="emplyeeid"
+                            label="员工id"
+                            rules={[{ required: true, message: '必须输入员工id' }]}
+                        >
+                            <Input placeholder="请输入员工id" />
+                        </Form.Item>
+                        <div style={{ display: method == '日结' ? 'block' : 'none' }}>
+                            <Form.Item name="workday" label="本月出勤">
+                                <InputNumber
+                                    placeholder="请输入本月出勤天数"
+                                    style={InputNumberStl}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                label="本月天数"
+                                name="daytotal"
+                                rules={[{ required: true, message: '必须输入本月天数' }]}
+                            >
+                                <InputNumber
+                                    min={0}
+                                    placeholder="请输入本月天数"
+                                    style={InputNumberStl}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                name="daysalary"
+                                label="日工资"
+                                rules={[{ required: true, message: '必须输入日工资' }]}
+                            >
+                                <InputNumber placeholder="请输入日工资" style={InputNumberStl} />
+                            </Form.Item>
+                            <Form.Item
+                                name="worklong"
+                                label="工龄"
+                                rules={[{ required: true, message: '必须输入工龄' }]}
+                            >
+                                <InputNumber placeholder="请输入工龄" style={InputNumberStl} />
+                            </Form.Item>
 
-                    <Form.Item
-                        name="daysalary"
-                        label="日工资"
-                        rules={[{ required: true, message: '必须输入日工资' }]}
-                    >
-                        <InputNumber placeholder="请输入日工资" />
-                    </Form.Item>
-                    <Form.Item
-                        name="worklong"
-                        label="工龄"
-                        rules={[{ required: true, message: '必须输入工龄' }]}
-                    >
-                        <InputNumber placeholder="请输入工龄" />
-                    </Form.Item>
+                            <Form.Item name="worklongmoney" label="工龄月薪">
+                                <InputNumber placeholder="请输入工龄月薪" style={InputNumberStl} />
+                            </Form.Item>
+                        </div>
 
-                    <Form.Item name="worklongmoney" label="工龄月薪">
-                        <InputNumber placeholder="请输入工龄月薪" />
-                    </Form.Item>
+                        <div style={{ display: method == '月结' ? 'block' : 'none' }}>
+                            <Form.Item name="workday" label="本月出勤">
+                                <InputNumber placeholder="请输入本月出勤天数" />
+                            </Form.Item>
+                            <Form.Item
+                                label="满勤天数"
+                                name="daytotal"
+                                rules={[{ required: true, message: '必须输入满勤天数' }]}
+                            >
+                                <InputNumber
+                                    min={0}
+                                    placeholder="请输入满勤天数"
+                                    style={InputNumberStl}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                name="fullsalary"
+                                label="满勤工资"
+                                rules={[{ required: true, message: '必须输入满勤工资' }]}
+                            >
+                                <InputNumber placeholder="请输入满勤工资" />
+                            </Form.Item>
+                            <Form.Item
+                                name="daysalary"
+                                label="日工资"
+                                rules={[{ required: true, message: '必须输入日工资' }]}
+                            >
+                                <InputNumber placeholder="请输入日工资" />
+                            </Form.Item>
+                            <Form.Item
+                                name="overdaysalary"
+                                label="超勤工资"
+                                rules={[{ required: true, message: '必须输入超勤工资' }]}
+                            >
+                                <InputNumber placeholder="请输入超勤工资" />
+                            </Form.Item>
+                        </div>
 
-                    <Form.Item
-                        label="本月天数"
-                        name="daytotal"
-                        rules={[{ required: true, message: '必须输入本月天数' }]}
-                    >
-                        <InputNumber min={0} placeholder="请输入本月天数" style={InputNumberStl} />
-                    </Form.Item>
-                </div>
+                        <div
+                            style={{
+                                display:
+                                    method == '无满勤奖励' || method == '按比例核定'
+                                        ? 'block'
+                                        : 'none',
+                            }}
+                        >
+                            <Form.Item name="workday" label="本月出勤">
+                                <InputNumber placeholder="请输入本月出勤天数" />
+                            </Form.Item>
+                            <Form.Item
+                                label="积累假期"
+                                name="holidays"
+                                rules={[{ required: true, message: '必须输入积累假期' }]}
+                            >
+                                <InputNumber
+                                    min={0}
+                                    placeholder="请输入积累假期"
+                                    style={InputNumberStl}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                name="monthholiday"
+                                label="每月休息天数"
+                                rules={[{ required: true, message: '必须输入月休息天数' }]}
+                            >
+                                <InputNumber placeholder="请输入月休息天数" />
+                            </Form.Item>
+                            <Form.Item
+                                name="factrestdays"
+                                label="实际休息天数"
+                                rules={[{ required: true, message: '必须输入实际休息天数' }]}
+                            >
+                                <InputNumber placeholder="请输入实际休息天数" />
+                            </Form.Item>
+                            <Form.Item
+                                name="daysalary"
+                                label="日工资"
+                                rules={[{ required: true, message: '必须输入日工资' }]}
+                            >
+                                <InputNumber placeholder="请输入日工资" />
+                            </Form.Item>
+                        </div>
 
-                <div style={{ display: method == '月结' ? 'block' : 'none' }}>
-                    <Form.Item name="workday" label="本月出勤">
-                        <InputNumber placeholder="请输入本月出勤天数" />
-                    </Form.Item>
-
-                    <Form.Item
-                        name="fullsalary"
-                        label="满勤工资"
-                        rules={[{ required: true, message: '必须输入满勤工资' }]}
-                    >
-                        <InputNumber placeholder="请输入满勤工资" />
-                    </Form.Item>
-                    <Form.Item
-                        name="daysalary"
-                        label="日工资"
-                        rules={[{ required: true, message: '必须输入日工资' }]}
-                    >
-                        <InputNumber placeholder="请输入日工资" />
-                    </Form.Item>
-                    <Form.Item
-                        name="overdaysalary"
-                        label="超勤工资"
-                        rules={[{ required: true, message: '必须输入超勤工资' }]}
-                    >
-                        <InputNumber placeholder="请输入超勤工资" />
-                    </Form.Item>
-                    <Form.Item
-                        label="满勤天数"
-                        name="daytotal"
-                        rules={[{ required: true, message: '必须输入满勤天数' }]}
-                    >
-                        <InputNumber min={0} placeholder="请输入满勤天数" style={InputNumberStl} />
-                    </Form.Item>
-                </div>
-
-                <div style={{ marginTop: 50, marginBottom: 80 }}>
-                    <Form.Item wrapperCol={{ span: 3, offset: 9 }}>
-                        <Button type="primary" htmlType="submit" block>
-                            完成提交
-                        </Button>
-                    </Form.Item>
-                </div>
-            </Form>
+                        <div style={{ marginTop: 30, marginBottom: 10 }}>
+                            <Form.Item wrapperCol={{ span: 3, offset: 9 }}>
+                                <Button type="primary" htmlType="submit" block>
+                                    完成提交3
+                                </Button>
+                            </Form.Item>
+                        </div>
+                    </Form>
+                </Collapse.Panel>
+                <Collapse.Panel header="员工薪水列表" key="2"></Collapse.Panel>
+            </Collapse>
         </Panel>
     )
 }
